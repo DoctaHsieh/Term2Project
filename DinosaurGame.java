cf6f9bf1a69ce0543369ed976deefd340fff4d4e
anuj0206 <120616551+anuj0206@users.noreply.github.com>
2022-12-25 14:39:16 -0600

    Update DinosaurGame.java

--- DinosaurGame.java
+++ DinosaurGame.java
@@ -2,8 +2,6 @@ import java.awt.Color;
 
 public class DinosaurGame {
 	public static void main(String[] args) {
-		LayeredSquares panel = new LayeredSquares();
-		panel.setBackground(Color.white);
-		panel.setVisible(true);
+
 	}
 }
