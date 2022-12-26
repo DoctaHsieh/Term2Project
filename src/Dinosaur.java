e3dcdcfd2022a3038628ecaa4288c394d7ce6743
anuj0206 <120616551+anuj0206@users.noreply.github.com>
2022-12-26 11:16:13 -0600

    Update Dinosaur.java

--- src/Dinosaur.java
+++ src/Dinosaur.java
@@ -1,6 +1,7 @@
 //This class creates the dinosaur for our game
 import java.awt.Color;
-
+import javax.swing.BorderFactory;
+import javax.swing.border.Border;
 public class Dinosaur extends ImgComponent {
 
 	private static final long serialVersionUID = 1L;
@@ -9,6 +10,8 @@ public class Dinosaur extends ImgComponent {
 		super.setBackground(Color.white);
 		super.setBounds(100, 335, 200, 165);
 		super.setImgFilePath("assets\\images\\dino.png");
+		Border emptyBorder = BorderFactory.createEmptyBorder();
+		super.setBorder(emptyBorder);
 
 	}
 
