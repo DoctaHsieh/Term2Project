a23b35b8ee7a3dcd38a5cad98188c7c9f0b03706
anuj0206 <120616551+anuj0206@users.noreply.github.com>
2022-12-26 11:16:49 -0600

    Update DinoObstacle.java

--- src/DinoObstacle.java
+++ src/DinoObstacle.java
@@ -1,6 +1,7 @@
 //This class creates the obstacle for our game
 import java.awt.Color;
-
+import javax.swing.BorderFactory;
+import javax.swing.border.Border;
 public class DinoObstacle extends ImgComponent {
 
 	private static final long serialVersionUID = 1L;
@@ -9,6 +10,7 @@ public class DinoObstacle extends ImgComponent {
 		super.setBackground(Color.white);
 		super.setBounds(800, 335, 151, 165);
 		super.setImgFilePath("assets\\images\\cactus2.png");
-
+		Border emptyBorder = BorderFactory.createEmptyBorder();
+		super.setBorder(emptyBorder);
 	}
 }
