c341146f9d9a012aefbce65773a1f7335d8d3af5
EvanSwanson05 <120616675+EvanSwanson05@users.noreply.github.com>
2022-12-26 19:06:48 -0600

    Update LayeredPanels.java
    Added KeyListener to jump when space is pressed

--- src/LayeredPanels.java
+++ src/LayeredPanels.java
@@ -1,24 +1,48 @@
 //This class places the components on panels
+import java.awt.Color;
 import javax.swing.JFrame;
 import javax.swing.JLayeredPane;
+import java.awt.event.*;
 
-public class LayeredPanels extends JFrame{
-
-	/**
-	 * 
-	 */
+public class LayeredPanels extends JFrame implements KeyListener{
 	private static final long serialVersionUID = 1L;
-//Place the components on different panels (different z indexes)
-	public LayeredPanels() {  
-	    setSize(1200, 650);  
-	    JLayeredPane pane = getLayeredPane();  
-	    //creating buttons  
-	    Dinosaur dinosaur = new Dinosaur();
+	Dinosaur dinosaur = new Dinosaur(); //Load dino
+    DinoObstacle obstacle = new DinoObstacle();  //Load cacti
+    DinoBackground background = new DinoBackground();  //Load ground
+    
+	//Place the components on different panels (different z indexes)
+	public LayeredPanels() throws InterruptedException {  
+		//Initialize window
+	    setSize(1200, 650);
+	    setResizable(false); // make height and width of window a defined number of pixels
+	    setTitle("Dino Game"); // set title
+	    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //terminate program when window closes
+	    setVisible(true);
+	    addKeyListener(this);
+	}
+	
+	public void loadScene() {
+	    JLayeredPane pane = getLayeredPane();
 	    
-	    DinoObstacle obstacle = new DinoObstacle();  
-	    DinoBackground background = new DinoBackground();  
+	    //create buttons
 	    pane.add(background, new Integer(1));  
 	    pane.add(obstacle, new Integer(2));  
 	    pane.add(dinosaur, new Integer(2));  
-	  }  
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if(e.getKeyCode() == 32) {
+			System.out.print("JUMP");
+			dinosaur.jump();
+		}
+	}
+	
+	
+	// Additional methods from KeyListener interface; not needed
+	@Override
+	public void keyTyped(KeyEvent e) {}
+	@Override
+	public void keyReleased(KeyEvent e) {}
 }
+
