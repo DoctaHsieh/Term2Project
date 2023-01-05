781d13c50941203528aa016a20a52d0607fbb371
DoctaHsieh <65909770+DoctaHsieh@users.noreply.github.com>
2023-01-04 19:11:44 -0600

    Score works now

--- src/DinosaurGame.java
+++ src/DinosaurGame.java
@@ -9,7 +9,9 @@ public class DinosaurGame{
 		
 		//Create window in which the game takes place
 		LayeredPanels panel = new LayeredPanels();
+		panel.running = true;
 		panel.loadScene();
 		panel.startGame();
+		panel.dinoPasses();
 	}
 }
